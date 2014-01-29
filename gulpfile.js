3ebc4c27a555fc1743b91d1e25898c68e84a6e55
morou <kazuhiro.nakae@gmail.com>
2014-01-30 08:56:56 +0900

    replace single quote to double quote

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 /* global require: true, console: true; */
-var gulp   = require('gulp');
-var uglify = require('gulp-uglify');
-var csso   = require('gulp-csso');
-var concat = require('gulp-concat');
-var clean  = require('gulp-clean');
-var exec   = require('child_process').exec;
+var gulp   = require("gulp");
+var uglify = require("gulp-uglify");
+var csso   = require("gulp-csso");
+var concat = require("gulp-concat");
+var clean  = require("gulp-clean");
+var exec   = require("child_process").exec;
 
 
 /***********************************************************************************
@@ -12,7 +12,7 @@ var exec   = require('child_process').exec;
  * default task
  *
  **********************************************************************************/
-gulp.task('default', ['chrome', 'opera', 'firefox', 'safari'], function() {
+gulp.task("default", ["chrome", "opera", "firefox", "safari"], function() {
 });
 
 
@@ -21,26 +21,26 @@ gulp.task('default', ['chrome', 'opera', 'firefox', 'safari'], function() {
  * task to make chrome extension
  *
  **********************************************************************************/
-gulp.task('chrome', function() {
-  var dest = 'dist/chrome';
+gulp.task("chrome", function() {
+  var dest = "dist/chrome";
 
   // javascript file
-  gulp.src(['src/loader.chrome.js', 'src/content.js'])
+  gulp.src(["src/loader.chrome.js", "src/content.js"])
       .pipe(uglify())
-      .pipe(concat('content.js'))
+      .pipe(concat("content.js"))
       .pipe(gulp.dest(dest));
 
   // css file
-  gulp.src(['style/content.css'])
+  gulp.src(["style/content.css"])
       .pipe(csso())
       .pipe(gulp.dest(dest));
 
   // other resource file
-  gulp.src(['contrib/icon/icon-16.png', 'contrib/icon/icon-48.png', 'contrib/icon/icon-128.png'])
+  gulp.src(["contrib/icon/icon-16.png", "contrib/icon/icon-48.png", "contrib/icon/icon-128.png"])
       .pipe(gulp.dest(dest));
 
   // create zip file
-  exec('zip -q dist/auto-load-new-posts-for-google-plus.zip dist/chrome/*');
+  exec("zip -q dist/auto-load-new-posts-for-google-plus.zip dist/chrome/*");
 });
 
 
@@ -49,22 +49,22 @@ gulp.task('chrome', function() {
  * task to make opera add-on
  *
  **********************************************************************************/
-gulp.task('opera', function() {
-  var dest = 'dist/opera';
+gulp.task("opera", function() {
+  var dest = "dist/opera";
 
   // javascript file
-  gulp.src(['src/loader.chrome.js', 'src/content.js']) // use same loader as chrome
+  gulp.src(["src/loader.chrome.js", "src/content.js"]) // use same loader as chrome
       .pipe(uglify())
-      .pipe(concat('content.js'))
+      .pipe(concat("content.js"))
       .pipe(gulp.dest(dest));
 
   // css file
-  gulp.src(['style/content.css'])
+  gulp.src(["style/content.css"])
       .pipe(csso())
       .pipe(gulp.dest(dest));
 
   // other resource file
-  gulp.src(['contrib/icon/icon-16.png', 'contrib/icon/icon-48.png', 'contrib/icon/icon-128.png'])
+  gulp.src(["contrib/icon/icon-16.png", "contrib/icon/icon-48.png", "contrib/icon/icon-128.png"])
       .pipe(gulp.dest(dest));
 });
 
@@ -74,24 +74,24 @@ gulp.task('opera', function() {
  * task to make firefox add-on
  *
  **********************************************************************************/
-gulp.task('firefox', function() {
-  var dest = 'dist/firefox/data';
+gulp.task("firefox", function() {
+  var dest = "dist/firefox/data";
 
   // javascript file
-  gulp.src(['src/loader.firefox.js', 'src/content.js'])
+  gulp.src(["src/loader.firefox.js", "src/content.js"])
       .pipe(uglify())
-      .pipe(concat('content.js'))
+      .pipe(concat("content.js"))
       .pipe(gulp.dest(dest));
 
   // css file
-  gulp.src(['style/content.css'])
+  gulp.src(["style/content.css"])
       .pipe(csso())
       .pipe(gulp.dest(dest));
 
   // create xpi file
-  exec('~/Library/Developer/Local/Firefox/addon-sdk/bin/cfx --pkgdir=firefox xpi', {cwd: 'dist'}, function(err) {
+  exec("~/Library/Developer/Local/Firefox/addon-sdk/bin/cfx --pkgdir=firefox xpi", {cwd: "dist"}, function(err) {
     if (err !== null) 
-      console.log('exec error: ' + err);
+      console.log("exec error: " + err);
   });
 });
 
@@ -101,22 +101,22 @@ gulp.task('firefox', function() {
  * task to make safari extension
  *
  **********************************************************************************/
-gulp.task('safari', function() {
-  var dest = 'dist/safari/autol-load-new-posts-for-google-plus.safariextension';
+gulp.task("safari", function() {
+  var dest = "dist/safari/autol-load-new-posts-for-google-plus.safariextension";
 
   // javascript file
-  gulp.src(['src/loader.safari.js', 'src/content.js'])
+  gulp.src(["src/loader.safari.js", "src/content.js"])
       .pipe(uglify())
-      .pipe(concat('content.js'))
+      .pipe(concat("content.js"))
       .pipe(gulp.dest(dest));
 
   // css file
-  gulp.src(['style/content.css'])
+  gulp.src(["style/content.css"])
       .pipe(csso())
       .pipe(gulp.dest(dest));
 
   // other resource file
-  gulp.src(['contrib/icon/icon-128.png'])
+  gulp.src(["contrib/icon/icon-128.png"])
       .pipe(gulp.dest(dest));
 });
 
@@ -126,7 +126,7 @@ gulp.task('safari', function() {
  * clean-up all
  *
  **********************************************************************************/
-gulp.task('clean', function() {
-  gulp.src(['dist/**/content.*', 'dist/**/*.png', 'dist/*.zip', 'dist/*.crx', 'dist/*.nex', 'dist/*.pem', 'dist/*.xpi', 'dist/*.safariextz'], {read: false})
+gulp.task("clean", function() {
+  gulp.src(["dist/**/content.*", "dist/**/*.png", "dist/*.zip", "dist/*.crx", "dist/*.nex", "dist/*.pem", "dist/*.xpi", "dist/*.safariextz"], {read: false})
       .pipe(clean());
 });
